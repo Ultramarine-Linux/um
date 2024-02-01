6eb0dd19eadb7a6b00872e9a8144cec399d43bee
madomado <madonuko@outlook.com>
2024-02-01 19:29:06 +0800

    feat(status): added actual implementations to os info, hw info and desktop info

--- status.go
+++ status.go
@@ -2,6 +2,10 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"os"
+	"regexp"
+	"strings"
 
 	"github.com/charmbracelet/lipgloss"
 	"github.com/jaypipes/ghw"
@@ -13,8 +17,8 @@ func statusInfo() ([][]string, error) {
 	dur, err := uptime.Get()
 	if err != nil {
 		return nil, err
-	}
 
+	}
 	//u := syscall.Utsname{}
 	//err = syscall.Uname(&u)
 	//if err != nil {
@@ -28,16 +32,89 @@ func statusInfo() ([][]string, error) {
 	}, nil
 }
 
+func get_from_os_release(content string, id string) string {
+	// I have no idea how to parse this properly
+	var regex = regexp.MustCompile(id + "=" + `[^\n]+`)
+	// apparently regexp does not support `^` to check if it starts on a new line using mode /m (multiline)
+	// so now we need to painfully loop through each line
+	var line string
+	for _, l := range strings.Split(content, "\n") {
+		line = regex.FindString(l)
+		// make sure the line starts with what we've found
+		if line != "" && strings.HasPrefix(l, line) {
+			break
+		}
+	}
+	// get rid of `KEY=` and quotes
+	out, _ := strings.CutPrefix(line, id+"=")
+	if strings.HasPrefix(out, `"`) && strings.HasSuffix(out, `"`) {
+		return out[1 : len(out)-1]
+	}
+	return out
+}
+
+func gather_os_info(listHeader, listItem func(strs ...string) string) (result []string, err error) {
+	file, err := os.Open("/etc/os-release")
+	if err != nil {
+		return
+	}
+	defer file.Close()
+	content, err := io.ReadAll(file)
+	if err != nil {
+		return
+	}
+	cont := string(content)
+	return []string{
+		listHeader("System"),
+		listItem("Name: " + get_from_os_release(cont, "NAME")),
+		listItem("Version: " + get_from_os_release(cont, "VERSION")),
+		listItem("Variant: " + get_from_os_release(cont, "VARIANT")),
+		listItem("Atomic: " + is_atomic()),
+	}, nil
+}
+
+func is_atomic() string {
+	_, err := os.Stat("/usr/bin/rpm-ostree")
+	if os.IsNotExist(err) {
+		return "False"
+	}
+	return "True"
+}
+
+func gather_hw_info(listHeader, listItem func(strs ...string) string) (result []string, err error) {
+	cpu, err := ghw.CPU()
+	if err != nil {
+		return
+	}
+	gpu, err := ghw.GPU()
+	if err != nil {
+		return
+	}
+
+	result = []string{
+		listHeader("Hardware"),
+		listItem("CPU: " + cpu.Processors[0].Model),
+	}
+	for i, card := range gpu.GraphicsCards {
+		result = append(result, listItem(fmt.Sprintf("GPU%d: %s", i, card.DeviceInfo.Product.Name)))
+		result = append(result, listItem(fmt.Sprintf("GPU%d Driver: %s", i, card.DeviceInfo.Driver))) //?
+	}
+	return
+}
+
+func gather_desktop(listHeader, listItem func(strs ...string) string) (result []string, err error) {
+	result = []string{
+		listHeader("Desktop"),
+		listItem("XDG_CURRENT_DESKTOP: " + os.Getenv("XDG_CURRENT_DESKTOP")),
+	}
+	return
+}
+
 func status(c *cli.Context) error {
 	//release, err := osrelease.Read()
 	//if err != nil {
 	//	return err
 	//}
-
-	cpu, err := ghw.CPU()
-	if err != nil {
-		return err
-	}
 
 	//fmt.Println(release["PRETTY_NAME"])
 
@@ -55,14 +132,22 @@ func status(c *cli.Context) error {
 
 	listItem := lipgloss.NewStyle().PaddingLeft(2).Render
 
-	fmt.Println(lipgloss.JoinVertical(lipgloss.Left,
-		listHeader("Ultramarine"), listItem("Variant: Flagship MEOWY"), listItem("Atomic: False MEOWY")))
+	osinfo, err := gather_os_info(listHeader, listItem)
+	if err != nil {
+		return err
+	}
+	fmt.Println(lipgloss.JoinVertical(lipgloss.Left, osinfo...))
 
-	fmt.Println(lipgloss.JoinVertical(lipgloss.Left,
-		listHeader("Hardware"), listItem(fmt.Sprintf("CPU: %s", cpu.Processors[0].Model)), listItem("GPU: NVIDIA MEOWY")))
+	hwinfo, err := gather_hw_info(listHeader, listItem)
+	if err != nil {
+		return err
+	}
+	fmt.Println(lipgloss.JoinVertical(lipgloss.Left, hwinfo...))
 
-	fmt.Println(lipgloss.JoinVertical(lipgloss.Left,
-		listHeader("Desktop"), listItem("Compositor: GNOME MEOWY")))
-
+	desktopinfo, err := gather_desktop(listHeader, listItem)
+	if err != nil {
+		return err
+	}
+	fmt.Println(lipgloss.JoinVertical(lipgloss.Left, desktopinfo...))
 	return nil
 }
