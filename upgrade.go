f4b4efd422ef47f50439a78ec450323fc86c651f
BROSKIs <71282752+BROSKIs@users.noreply.github.com>
2026-06-16 15:17:19 -0700

    Update upgrade.go
    Co-authored-by: nothingneko <me@jaiden.sh>

--- upgrade.go
+++ upgrade.go
@@ -80,7 +80,7 @@ func systemVersionUpgrade(c *cli.Context) error {
 	// Hook handling for the --check dry-run flag execution
 	if c.Bool("check") {
 		if currentVer == targetVer {
-			fmt.Printf("Your system is completely up-to-date on Ultramarine %s.\n", currentVer)
+			fmt.Printf("Your system is already up to date! Running Ultramarine %s.\n", currentVer)
 		} else {
 			fmt.Printf("A major system upgrade is available! Version %s -> %s\n", currentVer, targetVer)
 		}
